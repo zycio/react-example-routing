9f33b9e544b667389001e3ded73c078aa1dab3e5
Dariusz Futera <dariusz.futera@codibly.com>
2021-08-31 11:42:07 +0200

    Fixed routes.

--- src/ui/Layout/Navigation/Navigation.template.tsx
+++ src/ui/Layout/Navigation/Navigation.template.tsx
@@ -4,7 +4,7 @@ import "./Navigation.scss";
 
 export const NavigationTemplate = () => {
   const navigationElements = appRoutes.map(route => (
-    <NavLink exact to={route.path} key={route.name}>
+    <NavLink exact to={`${process.env.PUBLIC_URL}/react-example-routing/${route.path}`} key={route.name}>
       <img src={route.image} alt={route.name} />
       <span>{route.name}</span>
     </NavLink>
